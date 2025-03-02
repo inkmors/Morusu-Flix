ce1837a17a0029de821e80db971e2c4e5bd2bb7d
Vinicius Lima <gvlima.contato@gmail.com>
2025-03-02 03:46:17 -0300

    :bug: corrgindo bug do Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
       py-[2rem] px-[7rem] ">
         <div className="logo-align">
 
-            <Link className="flex items-end flex-col text-none" to="/home">
+            <Link className="flex items-end flex-col" to="/home">
                 <h1 className="font-[Mulish] font-[1000] text-[27px] text-[#f4b315]">MORUSU</h1>
                 <h1 className="font-[Josefinsans] font-[300] text-[20px] text-white">FLIX</h1>
             </Link>
@@ -17,7 +17,7 @@ function Header() {
         <Link className="favorites" to="/favorites"><p>Suspense</p></Link>
         <Link className="favorites" to="/favorites"><p>Em cartaz</p></Link> */}
 
-        <Link className="favorites bg-[#d8a011] px-[1rem] py-[0.5rem] rounded-[5px] font-[Josefinsans] font-[800] text-[17px] text-white text-none hover:bg-[#ffffff] hover:text-[#f4b315]" to="/favorites"><p>Favoritos</p></Link>
+        <Link className="favorites bg-[#d8a011] px-[1rem] py-[0.5rem] rounded-[5px] font-[Josefinsans] font-[800] text-[17px] text-white no-underline hover:bg-[#ffffff] hover:text-[#f4b315]" to="/favorites"><p>Favoritos</p></Link>
       </div>
     );
   }
